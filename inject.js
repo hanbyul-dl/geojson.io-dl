ecf634fb79c84bc75f07e73f7e85057c55e18824
hanbyul_dl <hanbyul@descarteslabs.com>
2018-05-03 15:25:15 -0400

    handling tilelayer better..?

--- inject.js
+++ inject.js
@@ -1,14 +1,47 @@
+let DLTileHandler = (function() {
+
+  var dlLayer = null;
+
+  function _destoryDLLayer() {
+    dlLayer.off('loading');
+    dlLayer.off('load');
+    window.api.map.removeLayer(dlLayer);
+    dlLayer = null;
+  }
+
+  function addDLLayer(tileID) {
+    if (dlLayer) {
+      _destoryDLLayer();
+    }
+
+    let tileTemplate =` https://platform.descarteslabs.com/raster/v2/xyz/${tileID}/{z}/{x}/{y}.png`;
+    dlLayer = L.tileLayer(tileTemplate);
+
+    dlLayer.on('tileloadstart', function tileLoadingStarted () {
+      console.log('DL tile loading started')
+    });
+
+    // When tile loading finished
+    dlLayer.on('load', function tileLoaded() {
+      console.log('DL tile loading ended')
+    });
+
+    window.api.map.addLayer(dlLayer);
+  }
+  return {
+    addDLLayer: addDLLayer
+  }
+})();
+
+
+
 document.addEventListener('CUSTOM_PRODUCT_SECLECTED', function(e) {
-  let dlTileLayer;
   console.log('from injectsed script: I got the event for CUSTOM PRODUCT SELECTED');
   var tileID = e.detail;
-  var tileTemplate =` https://platform.descarteslabs.com/raster/v2/xyz/${tileID}/{z}/{x}/{y}.png`;
-  if (dlTileLayer) {
-    dlTileLayer.setUrl(tileTemplate)
-  } else {
-    dlTileLayer= L.tileLayer(tileTemplate);
-    window.api.map.addLayer(dlTileLayer);
-  }
+
+  // It would be fantastic to know an ideal zoomoffset for each tileset
+  DLTileHandler.addDLLayer(tileID);
+
 });
 
 document.addEventListener('CUSTOM_GIST_SELECTED', function(e) {
